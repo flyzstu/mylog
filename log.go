0249cf01f44a5693314beb8e13c4f210865877b6
flyzstu <flyzstu@gmail.com>
2022-11-28 17:14:54 +0800

    add caller description

--- log.go
+++ log.go
@@ -4,6 +4,8 @@ package mylog
 
 import (
 	"fmt"
+	"path"
+	"runtime"
 	"time"
 )
 
@@ -25,16 +27,23 @@ type object struct {
 	level Level
 }
 
-// 日志通道
-var ch chan string
+var (
+	ch               chan string   // 日志通道
+	fi               chan struct{} // 结束信号通道
+	defaultLogObject *object       // 默认日志对象
+)
 
-// 结束通道
-var fi chan struct{}
+func logHelper() string {
+	_, file, line, _ := runtime.Caller(2)
+	file = path.Base(file)
+	return fmt.Sprintf("[%v: %v]", file, line)
+}
 
 // 初始化通道
 func init() {
 	ch = make(chan string, 100) // 缓冲区大小为100的通道
 	fi = make(chan struct{})    // 同步通道
+	defaultLogObject = new(object)
 	// 启动一个后台一直执行的goroutine，用来打印日志信息
 	go func() {
 		for msg := range ch {
@@ -44,7 +53,7 @@ func init() {
 	}()
 }
 
-func (o *object) Close() {
+func Close() {
 	close(ch) // 关闭通道ch
 	<-fi      // 阻塞主goroutine，直到通道内的数据发送完毕
 }
@@ -54,53 +63,48 @@ func getTimeNow() string {
 	return time.Now().Format("2006-01-02 15:04:05")
 }
 
-// 新建一个日志对象
-func New() *object {
-	return &object{}
-}
-
 // 设置日志级别
-func (o *object) SetLevel(level Level) {
-	o.level = level
+func SetLevel(level Level) {
+	defaultLogObject.level = level
 }
 
 // 检查日志级别是否需要打印
-func (o *object) checkLevel(level Level) bool {
-	return o.level <= level
+func checkLevel(level Level) bool {
+	return defaultLogObject.level <= level
 }
 
 // 打印INFO级别日志
-func (o *object) Info(msg string, a ...interface{}) {
-	if o.checkLevel(INFO) {
+func Info(msg string, a ...interface{}) {
+	if checkLevel(INFO) {
 		// 拼接字符串
-		msg = fmt.Sprintf("%s [INFO] %s", getTimeNow(), fmt.Sprintf(msg, a...))
+		msg = fmt.Sprintf("%s [INFO] %v %v", getTimeNow(), logHelper(), fmt.Sprintf(msg, a...))
 		ch <- msg
 	}
 }
 
 // 打印DEBUG级别日志
-func (o *object) Debug(msg string, a ...interface{}) {
-	if o.checkLevel(DEBUG) {
+func Debug(msg string, a ...interface{}) {
+	if checkLevel(DEBUG) {
 		// 拼接字符串
-		msg = fmt.Sprintf("%s [DEBUG] %s", getTimeNow(), fmt.Sprintf(msg, a...))
+		msg = fmt.Sprintf("%s [DEBUG] %v %v", getTimeNow(), logHelper(), fmt.Sprintf(msg, a...))
 		ch <- msg
 	}
 }
 
 // 打印WARNING级别日志
-func (o *object) Warning(msg string, a ...interface{}) {
-	if o.checkLevel(WARNING) {
+func Warning(msg string, a ...interface{}) {
+	if checkLevel(WARNING) {
 		// 拼接字符串
-		msg = fmt.Sprintf("%s [WARNING] %s", getTimeNow(), fmt.Sprintf(msg, a...))
+		msg = fmt.Sprintf("%s [WARNING] %v %v", getTimeNow(), logHelper(), fmt.Sprintf(msg, a...))
 		ch <- msg
 	}
 }
 
 // 打印ERROR级别日志
-func (o *object) Error(msg string, a ...interface{}) {
-	if o.checkLevel(ERROR) {
+func Error(msg string, a ...interface{}) {
+	if checkLevel(ERROR) {
 		// 拼接字符串
-		msg = fmt.Sprintf("%s [ERROR] %s", getTimeNow(), fmt.Sprintf(msg, a...))
+		msg = fmt.Sprintf("%s [ERROR] %v %v", getTimeNow(), logHelper(), fmt.Sprintf(msg, a...))
 		ch <- msg
 	}
 }
